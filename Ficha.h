cd341f4dfba9227ed586066ed79dd5942c05c1b0
Francisco Canseco <frank.canseco@gmail.com>
2015-10-21 16:16:13 -0500

    Clase Ficha
    Ficha Agregada

---
+++ Ficha.h
@@ -0,0 +1,13 @@
+//
+//  Ficha.h
+//  SargentoGorrito
+//
+//  Created by Francisco Canseco on 21/10/15.
+//  Copyright (c) 2015 Francisco Canseco. All rights reserved.
+//
+
+#ifndef SargentoGorrito_Ficha_h
+#define SargentoGorrito_Ficha_h
+
+
+#endif
